169d494c9811233ee21d46fe4654d4fcd22b87a1
Your Name <your.email@example.com>
2024-12-27 19:17:52 +0630

    fix: configure polyfills and optimize webpack bundling

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
-  transpileDependencies: true,
-  css: {
-    loaderOptions: {
-      sass: {
-        additionalData: `
-          @import "@/assets/styles/_variables.scss";
-        `
-      }
-    }
-  },
   chainWebpack: config => {
     config.plugin('html').tap(args => {
-      args[0].title = '၁၈ကရက် ချက်'
       args[0].favicon = 'public/18klogo.jpg'
       return args
     })
