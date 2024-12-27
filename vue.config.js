49892cd92b7e59ee49a6e8933f88650d3257ac3e
Your Name <your.email@example.com>
2024-12-27 19:25:31 +0630

    fix: add global SCSS variables configuration

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
+  css: {
+    loaderOptions: {
+      scss: {
+        additionalData: `@import "@/assets/styles/_variables.scss";`
+      }
+    }
+  },
   chainWebpack: config => {
     config.plugin('html').tap(args => {
       args[0].favicon = 'public/18klogo.jpg'
